f854c919be4ea3bc3a20cf3c3de0e4a2fc32a603
TecXCTO <cto.tecx@gmail.com>
2026-02-18 14:27:46 +0530

    Update setup_term.sh

--- setup_term.sh
+++ setup_term.sh
@@ -1,3 +1,6 @@
 pkg update && pkg upgrade
 pkg install python python-pip clang make cmake ninja libandroid-execinfo libffi openssl
 
+curl -LO https://its-pointless.github.io
+bash setup-pointless-repo.sh
+pkg install python-torch
