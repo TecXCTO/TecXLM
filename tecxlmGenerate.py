1dc7ae53d5a8c46b0dfcb467204bff10785cf850
TecXCTO <cto.tecx@gmail.com>
2026-04-04 09:25:36 +0530

    Update tecxlmGenerate.py

--- tecxlmGenerate.py
+++ tecxlmGenerate.py
@@ -5,16 +5,22 @@ from tecxlm import TecXModel
 model_path = Path("tecxlm") / "TecXLM.pth"
 
 #model = TecXModel().generate()
+print(tecxmodelgen creating)
 class TecXModelGen(TecXModel):
   def init():
     super.init()
+print(tecxmodelgen created)
 model = TecXModelGen()
+print(model veriable creating)
 # torch.save(model.state_dict(),"../TecXLM.pth")
 model.load_state_dict(torch.load(model_path))
-
+print(model veriable creating)
 model.to(device)
 model.eval()
+print(model veriable evaluated)
 context = torch.zeros((1, 1), dtype=torch.long, device=device)
+print(context veriable created)
 print(decode(model.generate(context, max_new_tokens=500)[0].tolist()))
+print(model generating completed.)
 #open('more_generated.txt', 'w').write(decode(model.generate(context, max_new_tokens=10000)[0].tolist()))
 open('TecXLM_generating.txt', 'w').write(decode(model.generate(context, max_new_tokens=10000)[0].tolist()))
