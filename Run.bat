daa4275e84b11174de20290dea9a49d00db622cd
TecXCTO <cto.tecx@gmail.com>
2026-04-04 08:49:49 +0530

    Update Run.bat

--- Run.bat
+++ Run.bat
@@ -1,2 +1,5 @@
-python -m pip install -r requirements.txt
-python tecxlm.py --out_dir=out-TecXLM_learn
+python tecxlmgenerate.py --out_dir=tecxlm-outputs
+pause
+run
+# python -m pip install -r requirements.txt
+# python tecxlm.py --out_dir=out-TecXLM_learn
