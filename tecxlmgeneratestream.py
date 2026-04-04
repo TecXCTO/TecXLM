2398708f405590ed11401f7e2ad4db106e95c6a0
TecXCTO <cto.tecx@gmail.com>
2026-04-04 23:48:11 +0530

    Update tecxlmgeneratestream.py

--- tecxlmgeneratestream.py
+++ tecxlmgeneratestream.py
@@ -7,7 +7,8 @@ from tecxlm import TecXModel
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 # Define the exact 71 characters
-chars = string.ascii_lowercase + string.ascii_uppercase + string.digits + " !.,:;?-\n"
+#chars = string.ascii_lowercase + string.ascii_uppercase + string.digits + " !.,:;?-\n"
+chars = "!,.:?CEFHILMTUWXYabcdefghiklmnopqrstuvwxy"
 chars = sorted(list(set(chars)))
 stoi = { ch:i for i,ch in enumerate(chars) }
 itos = { i:ch for i,ch in enumerate(chars) }
