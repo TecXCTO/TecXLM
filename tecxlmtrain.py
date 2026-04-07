7de156e9b9bb85f5de39eb2a3e52ff57e4a59cb1
TecXCTO <cto.tecx@gmail.com>
2026-04-07 17:56:19 +0530

    Update tecxlmtrain.py

--- tecxlmtrain.py
+++ tecxlmtrain.py
@@ -20,7 +20,7 @@ dropout = 0.2
 torch.manual_seed(1337)
 
 # wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
-with open('inputs/inputs.txt', 'r', encoding='utf-8') as f:
+with open('inputs/inputs.txt', 'r', encoding='utf-8-sig') as f:
     text = f.read()
 
 # here are all the unique characters that occur in this text
