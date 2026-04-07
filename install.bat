7f0fe61303b2204267bbd186d41c59e9be30d330
TecXCTO <cto.tecx@gmail.com>
2026-04-07 18:13:26 +0530

    Create install.bat

---
+++ install.bat
@@ -0,0 +1,8 @@
+# For PyTorch to ONNX (built-in, but need ONNX library)
+pip install onnx
+
+# For TensorFlow to ONNX
+pip install tf2onnx
+
+# To run and test the converted models on your PC or Raspberry Pi
+pip install onnxruntime
