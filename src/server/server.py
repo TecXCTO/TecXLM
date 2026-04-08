271baf55e3691d5532184ed52eb36a4e75801212
TecXCTO <cto.tecx@gmail.com>
2026-04-08 10:35:01 +0530

    Update server.py

--- src/server/server.py
+++ src/server/server.py
@@ -51,3 +51,10 @@ if __name__ == "__main__":
     #uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
     uvicorn.run("app", host="127.0.0.1", port=8000, reload=True)
     
+"""
+uvicorn main:app --reload
+
+uvicorn src.server.server:app --reload
+python -m uvicorn src.server.server:app --reload
+
+"""
